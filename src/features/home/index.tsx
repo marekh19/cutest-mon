dbbbcc926f73f79e55fa4d943be671daba3f7fac
Marek Honzal <marek.honzal@gmail.com>
2022-08-15 00:09:57 +0200

    feat: added tRPC and tested dummy route

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -2,20 +2,28 @@ import type { FC } from 'react'
 
 import { Layout, H1, DuelWrapper } from './styled'
 import { PokemonCard } from './parts/PokemonCard'
+import { trpc } from '~/utils/trpc'
 
-export const Homepage: FC = () => (
-  <Layout>
-    <H1>Which Pokémon is Rounder?</H1>
-    <DuelWrapper>
-      <div>
-        <PokemonCard />
-        <button>Roundest</button>
-      </div>
-      <p>vs.</p>
-      <div>
-        <PokemonCard />
-        <button>Roundest</button>
-      </div>
-    </DuelWrapper>
-  </Layout>
-)
+export const Homepage: FC = () => {
+  const { data, isLoading } = trpc.useQuery(['hello', { text: 'Marek' }])
+
+  if (isLoading) return <p>Loading...</p>
+
+  if (data) return <p>{data.greeting}</p>
+  return (
+    <Layout>
+      <H1>Which Pokémon is Rounder?</H1>
+      <DuelWrapper>
+        <div>
+          <PokemonCard />
+          <button>Roundest</button>
+        </div>
+        <p>vs.</p>
+        <div>
+          <PokemonCard />
+          <button>Roundest</button>
+        </div>
+      </DuelWrapper>
+    </Layout>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,4 +10,43 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+import { withTRPC } from '@trpc/next'
+import type { AppRouter } from '~/backend/router'
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return ''
+  }
+  // reference for vercel.com
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  // reference for render.com
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
+  }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+      /**
+       * @link https://react-query-v3.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    }
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp)
