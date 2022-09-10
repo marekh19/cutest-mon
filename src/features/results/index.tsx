ad1f849a84ceca087aae730658517abaff6e0bb5
Marek Honzal <marek.honzal@gmail.com>
2022-09-11 00:07:29 +0200

    feat: prepared new page for results

---
+++ src/features/results/index.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react'
+
+export const Results: FC = () => {
+  return <h1>Results</h1>
+}
---
+++ src/pages/results.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+import { Results as ResultsPage } from '~/features/results'
+
+const Results: NextPage = () => {
+  return <ResultsPage />
+}
+
+export default Results
