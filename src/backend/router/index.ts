5984b970469d1012dca17fa38c67848fd474d55f
Marek Honzal <marek.honzal@gmail.com>
2022-09-13 21:43:25 +0200

    feat: updated router + added favicon files

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,21 +1,23 @@
 import * as trpc from '@trpc/server'
 import { z } from 'zod'
-
-import { PokemonClient } from 'pokenode-ts'
 import { prisma } from '~/backend/utils/prisma'
+import { getOptionsForVote } from '~/utils/getRandomPokemon'
 
 export const appRouter = trpc
   .router()
-  .query('get-pokemon-by-id', {
-    input: z.object({ id: z.number() }),
-    async resolve({ input }) {
-      const pokemon = await prisma.pokemon.findFirst({
-        where: { id: input.id },
+  .query('get-pokemon-pair', {
+    async resolve() {
+      const [first, second] = getOptionsForVote()
+
+      const bothPokemon = await prisma.pokemon.findMany({
+        where: { id: { in: [first, second] } },
       })
 
-      if (!pokemon) throw new Error('Pokemon does not exist.')
+      if (bothPokemon.length !== 2) {
+        throw new Error('Failed to find two pokemon')
+      }
 
-      return pokemon
+      return { firstPokemon: bothPokemon[0], secondPokemon: bothPokemon[1] }
     },
   })
   .mutation('cast-vote', {
